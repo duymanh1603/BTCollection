c2d1ef49516abbfc7cead6ae2a2786875f1fffa4
duymanh1603 <duymanh160322@gmail.com>
2025-08-14 13:30:33 +0700

    update

--- src/com/data/BaiTap/Bai7.java
+++ src/com/data/BaiTap/Bai7.java
@@ -12,14 +12,30 @@ public class Bai7 {
         System.out.print("Nhập số lượng trường đại học: ");
         int n = sc.nextInt();
         sc.nextLine();
+
         for (int i = 0; i < n; i++) {
             String name;
             while (true) {
                 System.out.print("Nhập tên trường thứ " + (i + 1) + ": ");
-                name = sc.nextLine();
+                name = sc.nextLine().trim();
 
-
+                if (name.isEmpty()) {
+                    System.out.println("Tên trường không được để trống. Nhập lại!");
+                } else if (name.matches("\\d+")) {
+                    System.out.println("Tên trường không được chỉ chứa số. Nhập lại!");
+                } else {
+                    universities.add(name);
+                    break;
+                }
             }
         }
+
+        // In danh sách
+        System.out.println("\nDanh sách các trường đại học:");
+        for (String uni : universities) {
+            System.out.println("- " + uni);
+        }
+
+        sc.close();
     }
 }
